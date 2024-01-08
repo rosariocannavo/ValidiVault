996af1088e994dd63162caed1d8c65ab90075d8a
Rosario Cannavò <rosariocannavo2000@gmail.com>
2024-01-08 11:14:00 +0100

    fix: "fixed cb logging"

--- api_gateway/internal/circuit_breaker/circuit_breaker_config.go
+++ api_gateway/internal/circuit_breaker/circuit_breaker_config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/rosariocannavo/api_gateway/internal/nats"
 	"github.com/sony/gobreaker"
 )
 
@@ -11,7 +12,7 @@ var CircuitBreaker *gobreaker.CircuitBreaker
 
 func init() {
 	CircuitBreaker = gobreaker.NewCircuitBreaker(gobreaker.Settings{
-		Name:        "My Circuit Breaker",
+		Name:        "Circuit Breaker",
 		MaxRequests: 0,               // Maximum number of consecutive failures before tripping the circuit
 		Interval:    5 * time.Second, // Duration to wait before allowing another request
 		Timeout:     2 * time.Second, // Timeout for a single request attempt
@@ -22,6 +23,8 @@ func init() {
 		},
 		OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
 			fmt.Printf("Circuit breaker '%s' changed from '%s' to '%s'\n", name, from, to)
+			message := fmt.Sprintf("Timestamp: %s | Circuit breaker '%s' changed from '%s' to '%s'\n", time.Now().UTC().Format(time.RFC3339), name, from, to)
+			nats.NatsConnection.PublishMessage(message)
 		},
 	})
 }
